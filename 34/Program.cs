f7c7a82095d7927cb60257772e69332ab1a75977
aanatyrnal <cacina_ang1891@mail.ru>
2023-09-12 13:16:30 +0300

    add task 34

---
+++ 34/Program.cs
@@ -0,0 +1,25 @@
+﻿Random random = new Random();
+
+int[] array = new int[4]; 
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(100, 1000);
+}
+
+Console.WriteLine("Массив:");
+foreach (int num in array)
+{
+    Console.WriteLine(num);
+}
+
+int evenCount = 0;
+foreach (int num in array)
+{
+    if (num % 2 == 0)
+    {
+        evenCount++;
+    }
+}
+
+Console.WriteLine(evenCount);
