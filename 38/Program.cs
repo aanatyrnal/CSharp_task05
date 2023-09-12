a380020720982fc8895bdbc773029b24e9deb6f7
aanatyrnal <cacina_ang1891@mail.ru>
2023-09-12 13:33:43 +0300

    add task 38

---
+++ 38/Program.cs
@@ -0,0 +1,32 @@
+﻿Random random = new Random();
+double[] array = new double[4];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.NextDouble() * (200) - 100; // Генерируем числа в диапазоне [-100, 100)
+}
+
+Console.WriteLine("Массив:");
+foreach (double num in array)
+{
+    Console.WriteLine(num);
+}
+
+double min = array[0];
+double max = array[0];
+
+for (int i = 1; i < array.Length; i++)
+{
+    if (array[i] < min)
+    {
+        min = array[i];
+    }
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+}
+
+double difference = max - min;
+
+Console.WriteLine($"Разница: {difference}");
